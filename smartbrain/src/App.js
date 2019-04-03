1a999cc74b01ba22c902e243526b5055e4952e9b
LenaNguyen27 <nguyen.lena27@hotmail.com>
2019-04-02 21:52:18 -0400

    created main components

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Clarifai from 'clarifai';
+import Navigation from './components/Navigation/Navigation';
+import Particles from 'react-particles-js';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from  './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+
+const app = new Clarifai.App({
+  apiKey: '82494a1a16534019983a7a152eb80634'
+});
+
+const ParticlesOptions = {
+    "particles": {
+      "number": {
+          "value": 100,
+          "density":{
+            "enable": true,
+            "value-area": 800
+          }
+      },
+      "move": {
+          "speed": 5
+      }
+  }
+}
 
 class App extends Component {
+constructor(){
+  super();
+  this.state = {
+    input: '',
+  }
+}
+
+onInputChange = (event) => {
+  console.log(event.target.value);
+}
+
+onButtonSubmit = () => {
+  app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+}
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Particles className='particles' params={ParticlesOptions} />
+        <Navigation/>
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        {
+          // <FaceRecognition/>
+        }
       </div>
     );
   }
---
+++ smartbrain/src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () =>{
+    return(
+        <div className="dib">
+            <div className="white f3">
+                {'Lena, your current rank is...'}
+            </div>
+            <div className="white f1">
+                {'5'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
