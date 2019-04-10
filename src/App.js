3514d18b677c53d514567cdf82c95c574c5b9a96
LenaNguyen27 <nguyen.lena27@hotmail.com>
2019-04-10 19:29:11 -0400

    integrated back end with register

--- src/App.js
+++ src/App.js
@@ -37,8 +37,26 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: 0,
+      joined: new Date()
+    }
+  }); 
   }
 
   calculateFaceLocation = (data) => {
@@ -90,14 +108,14 @@ class App extends Component {
         {this.state.route === 'home'
           ? <div>
           <Logo />
-          <Rank />
+          <Rank name={this.state.user.name} entries={this.state.user.entries}/>
           <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
           <FacialRecognition box={this.state.box} imageUrl={imageUrl} />
           </div>
         : (
             this.state.route === 'signin'
-            ? <SignIn onRouteChange={this.onRouteChange} />
-            : <Register onRouteChange={this.onRouteChange} />
+            ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           )
           }
       </div>
