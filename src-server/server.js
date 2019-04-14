bf7fbd0fb1cd3e96f38bb5be7dcf3e6e19b76d56
LenaNguyen27 <nguyen.lena27@hotmail.com>
2019-04-14 14:50:36 -0400

    modified PORT

--- src-server/server.js
+++ src-server/server.js
@@ -37,7 +37,6 @@ app.put('/image', (req, res) => image.handleImage(req, res, db));
 
 app.post('/imageUrl', (req, res) => {image.handleApiCall(req, res)});
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-    console.log(`app is running on port ${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 });
