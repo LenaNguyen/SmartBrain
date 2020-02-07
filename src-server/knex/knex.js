e0d2b20a00f73e709e09a93965c4a5e8a18d0b10
LenaNguyen27 <nguyen.lena27@hotmail.com>
2020-02-07 10:06:47 -0500

    fix git error

--- src-server/knex/knex.js
+++
@@ -1,5 +0,0 @@
-const knexConfig = require("../knexfile");
-
-const environment = process.env.NODE_ENV || "development";
-const config = knexConfig[environment];
-module.exports = require("knex")(config);
