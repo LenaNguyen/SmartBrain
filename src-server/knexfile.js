c93241ee9211b12613c1f107023126f10a7574c5
LenaNguyen27 <nguyen.lena27@hotmail.com>
2020-02-07 10:04:07 -0500

    remove config

--- src-server/knexfile.js
+++ src-server/knexfile.js
@@ -5,8 +5,8 @@ module.exports = {
     client: "pg",
     connection: {
       host: "127.0.0.1",
-      user: "postgres",
-      password: "test",
+      user: "",
+      password: "",
       database: "smartbrain"
     },
     migrations: {
