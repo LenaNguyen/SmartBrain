2b946863036381d028852cbea648fb94fbd00a84
LenaNguyen27 <nguyen.lena27@hotmail.com>
2020-02-06 22:31:09 -0500

    url update

--- src-client/src/settings/appConfig.js
+++ src-client/src/settings/appConfig.js
@@ -1,6 +1,5 @@
 const Settings = {
-  // baseUrl: 'https://obscure-lowlands-16603.herokuapp.com/'
-  baseUrl: "http://localhost:3000/",
+  baseUrl: "https://obscure-lowlands-16603.herokuapp.com/",
   galleryImageWidth: 200
 };
 
